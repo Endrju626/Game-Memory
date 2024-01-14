44fb75bd6d1954cc2763ee935b7689d6f35b7b76
Andrzej Macura <aandrzej15@gmail.com>
2024-01-14 19:07:05 +0100

    Added and configured Vue.js in the project

---
+++ memory/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<h1>{{ title }}</h1>
+
+<div class="Game">
+
+</div>
+
+<button @click="Play">
+  Play!
+</button>
+
+<button @click="Reset">
+  Reset
+</button>
+
+<div class="Timer">
+  Time:
+</div>
+
+       Name Time
+Ranking: 
+</template>
+
+<script>
+
+
+export default {
+  data() {
+ return{
+  title:'Memory Game'
+    }
+  },
+  methods: {
+    Play(){
+      alert('start')
+    },
+    Reset(){
+      alert('no')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
