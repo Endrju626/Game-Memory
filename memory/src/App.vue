1266074078118cc161ae66a24187e8902c92e80c
Andrzej Macura <aandrzej15@gmail.com>
2024-01-16 20:19:26 +0100

    Add images

--- memory/src/App.vue
+++ memory/src/App.vue
@@ -1,15 +1,16 @@
 <template>
 <h1>{{ title }}</h1>
 
-<div class="Game">
+<div v-for="(image, index) in images" :key="index" class="Game">
+<img :src="image" alt="memory pic" width="256px" height="256px"/>
 
 </div>
 
-<button @click="Play">
+<button class="Play" @click="Play">
   Play!
 </button>
 
-<button @click="Reset">
+<button class="Reset" @click="Reset">
   Reset
 </button>
 
@@ -19,6 +20,7 @@
 
        Name Time
 Ranking: 
+<!-- <img src="../../Images/car.jpg"> -->
 </template>
 
 <script>
@@ -27,7 +29,25 @@ Ranking:
 export default {
   data() {
  return{
-  title:'Memory Game'
+  title:'Memory Game',
+  images:[
+      "images/car.jpg" ,
+      "images/car.jpg",
+      "images/Chameleon.jpg",
+      "images/Chameleon.jpg",
+      "images/Eclipse.png",
+      "images/Eclipse.png",
+      "images/Microscope.jpg",
+      "images/Microscope.jpg",
+      "images/Nebula.jpg",
+      "images/Nebula.jpg",
+      "images/Pyramid.png",
+      "images/Pyramid.png",
+      "images/Sun.jpg",
+      "images/Sun.jpg",
+      "images/Whale.jpg",
+      "images/Whale.jpg"
+  ]
     }
   },
   methods: {
@@ -42,5 +62,61 @@ export default {
 </script>
 
 <style>
+*
+{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+font-family: monospace;
+}
+body
+{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #0a3c2f;
+}
+
+#app{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 15px;
+  background: #0d614b;
+  padding: 40px 60px;
+}
+
+h2{
+  font-size: 3em;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+}
+
+.Reset{
+  padding: 15px 20px;
+  color: #267c65;
+  background: #fff;
+  border: none;
+  font-size: 1.5em;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-weight: 600;
+}
+
+.Reset:focus{
+  background-color:  #fff;
+  font-weight: 500;
+
+}
+
+.Game {
+ gap: 15px;
+}
+
 
 </style>
